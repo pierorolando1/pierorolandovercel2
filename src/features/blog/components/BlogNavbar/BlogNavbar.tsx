58bf1bab620bf1eac8d175fcb01ba7f25b379449
Piero Rolando <rolandopiero63@gmail.com>
2022-05-09 22:59:26 -0400

    improve navbar styles

--- src/features/blog/components/BlogNavbar/BlogNavbar.tsx
+++ src/features/blog/components/BlogNavbar/BlogNavbar.tsx
@@ -1,31 +1,36 @@
 import { Tabs, useTheme } from "@geist-ui/core"
 import styled from "styled-components"
+import ChangeThemeDropdown from "../../../geist-theme/components/ChangeThemeDropdown"
 
 const BlogNavbar = styled.nav`
 position: fixed;
 top: 0;
 width: 100%;
 max-width: 65.5rem;
+display: flex;
+justify-content: space-between;
+align-items: center;
 
 background-color: ${
     // @ts-ignore
     (props) => props.background
     };
 
-animation: fadeIn 0.25s ease-in;
+animation: fadeIn 0.25s ease;
 
 @keyframes fadeIn {
     from {
+        top: -10px;
         opacity: 0;
     }
     to {
+        top: 0;
         opacity: 1;
     }
 }
 `
 
 export default () => {
-
     const { palette: { background } } = useTheme()
     return (
         //@ts-ignore
@@ -34,6 +39,7 @@ export default () => {
                 <Tabs.Item value="1" label={<>All</>} />
                 <Tabs.Item value="2" label={<>Tutorials</>} />
             </Tabs>
+            <ChangeThemeDropdown />
         </BlogNavbar>
     )
 }
