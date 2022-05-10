c955a14b3e740077251b2194364ba6091cb8dbe8
Piero Rolando <rolandopiero63@gmail.com>
2022-05-09 23:01:29 -0400

    add colors for default theme (dark grey)

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,23 +2,31 @@ import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 import { CssBaseline, GeistProvider, Themes } from '@geist-ui/core'
 import Navbar from '../features/ui/components/Navbar'
+import { useGeistTheme } from '../features/geist-theme/zustand/useStoreThemeGeist'
 
 const defaultTheme = Themes.createFromDark({
-    type: "defaultTheme",
+    type: "default",
     palette: {
-        background: "#0f101a"
+        background: "#0f101a",
+        accents_1: "#1b1c2e",
+        accents_2: "#272943",
+        accents_3: "#313354",
+        accents_4: "#3b3e66",
+        accents_5: "#464978",
+        accents_6: "#50548a",
+        accents_7: "#5b5f9c",
+        accents_8: "#6a6fa8",
+        foreground: "#cbcde1"
     }
 })
 
-const darkTheme = Themes.createFromDark({ type: "darkTheme" })
-const lightTheme = Themes.createFromLight({ type: "lightTheme" })
-
 function MyApp({ Component, pageProps }: AppProps) {
+    const { currentTheme } = useGeistTheme()
+
     return (
-        <GeistProvider themes={[defaultTheme, darkTheme, lightTheme]} themeType="dark">
+        <GeistProvider themes={[defaultTheme]} themeType={currentTheme}>
             <CssBaseline />
-            <div style={{
-            }}>
+            <div>
                 <Navbar />
                 <section style={{ maxWidth: "65rem", margin: "0 auto" }}>
                     <Component {...pageProps} />
