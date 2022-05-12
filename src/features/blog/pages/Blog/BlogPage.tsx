d7255981af7bd1df8583f96f0ab6d0e9c32647f7
Piero Rolando <rolandopiero63@gmail.com>
2022-05-11 22:46:37 -0400

    The page gets the data on the server and pass to BlogPage component as a prop

--- src/features/blog/pages/Blog/BlogPage.tsx
+++ src/features/blog/pages/Blog/BlogPage.tsx
@@ -1,36 +1,12 @@
-import { AutoComplete, Spacer, Text } from "@geist-ui/core"
-import { Heart } from '@geist-ui/icons'
-import styled from "styled-components"
+import { AutoComplete, Spacer } from "@geist-ui/core"
+
 import BlogNavbar from "../../components/BlogNavbar"
+import BlogPostCard from "../../components/BlogPostCard"
+import { PostType } from "../../types/post"
 import { useStoreBlog } from "../../zustand/navbarBlog"
 
-const Button = styled.button`
-display: flex;
-flex-direction: column;
-border: none;
 
-padding: 0.5rem 0.8rem;
-
-background-color: transparent;
-cursor: pointer;
-transition: all 0.2s ease;
-
-border-radius: 5px;
-
-&:hover {
-    background-color: rgba(255, 255, 255, 0.009);
-}
-
-`
-
-const Card = styled.article`
-max-width: 49rem;
-border-bottom: 1px solid rgba(255, 255, 255, 0.03);
-padding: 1.9rem 0;
-`
-
-
-export default () => {
+export default function BlogPage({ posts }: { posts: PostType[] }) {
 
     const show = useStoreBlog(state => state.showBlogBar)
 
@@ -45,54 +21,13 @@ export default () => {
                 <Spacer />
                 <AutoComplete options={[]} placeholder="Search..." onSearch={() => { }} />
                 <Spacer />
-                <Card>
-                    <div
-                        style={{
-                            display: "flex",
-                            flexWrap: "nowrap",
-                        }}
-                    >
 
-                        <Spacer w={0.8} />
-                        <div style={{
-                            width: "100%",
-                        }}>
-                            <Text h4>How to be happy</Text>
-                            <Text h6 style={{ opacity: "0.6" }}>Lorem ipsum idk what more adjsajdhsa </Text>
-                        </div>
-                        <div>
-                            <Button>
-                                <Heart size={18} />
-                                16
-                            </Button>
-                        </div>
+                {
+                    posts.map(post => {
+                        return <BlogPostCard key={post.id} post={post} />
+                    })
+                }
 
-                    </div>
-                </Card>
-                <Card>
-                    <div
-                        style={{
-                            display: "flex",
-                            flexWrap: "nowrap",
-                        }}
-                    >
-
-                        <Spacer w={0.8} />
-                        <div style={{
-                            width: "100%",
-                        }}>
-                            <Text h4>How to be happy</Text>
-                            <Text h6 style={{ opacity: "0.6" }}>Lorem ipsum idk what more adjsajdhsa </Text>
-                        </div>
-                        <div>
-                            <Button>
-                                <Heart size={18} />
-                                16
-                            </Button>
-                        </div>
-
-                    </div>
-                </Card>
             </div>
         </>
     )
--- src/pages/blog/index.tsx
+++ src/pages/blog/index.tsx
@@ -1,1 +1,23 @@
-export { default } from '../../features/blog/pages/Blog'
+import { FC } from 'react'
+import BlogPage from '../../features/blog/pages/Blog'
+import type { PostType } from '../../features/blog/types/post'
+
+export const getServerSideProps: () => Promise<{ props: { posts: PostType[] } }> = async () => {
+
+    //TODO: fetch posts from database
+
+    return {
+        props: {
+            posts: [{
+                title: "Hello World",
+                description: "This is a test blog post",
+
+            } as PostType]
+        }
+    }
+}
+
+const Blog: FC<{ posts: PostType[] }> = ({ posts }) => <BlogPage posts={posts} />
+
+
+export default Blog
